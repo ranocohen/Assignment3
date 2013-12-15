0b75afd06841e993ffe89a03172e4644a1c4c628
Idan <idanakav@gmail.com>
2013-12-15 10:35:42 +0200

    Added javadoc to Warehouse class

--- Assignment3-github/src/com/bgu/assignment3/Warehouse.java
+++ Assignment3-github/src/com/bgu/assignment3/Warehouse.java
@@ -1,41 +1,84 @@
 package com.bgu.assignment3;
 
+
+/**
+ * 
+ * Warehouse class holding our ingredients and kitchen tools
+ *
+ */
 public class Warehouse implements WarehouseActions{
 	
 	public Warehouse() {
 		
 	}
-
+	/**
+	 * Simulates acquiring @{link KitchenTool} from the Warehouse
+	 * 
+	 * @param name - kitchen tool name
+	 * @param quantity - quantity to take
+	 */
 	public void takeKitchenTool(String name, int quantity) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	/**
+	 * Simulating returning @{link KitchenTool} to the Warehouse
+	 * 
+	 * @param name - kitchen tool name
+	 * @param quantity - quantity to take
+	 */
 	public void returnKitchenTool(String name, int quantity) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	/**
+	 * Adds a new @{link KitchenTool} to the Warehouse
+	 * 
+	 * @param name - the kitchen tool name
+	 * @param quantity - the kitchen tool quantity
+	 */
 	public void addKitchenTool(String name, int quantity) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	/**
+	 * Gets the current quantity of specific @{link KitchenTool}
+	 * 
+	 * @param name - the kitchen tool name
+	 * 
+	 * @return the quantity of kitchen tool named {@code name}
+	 */
 	public int getKitchenToolQuantity(String name) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	/**
+	 * Gets the current quantity of specific {@link Ingredient}
+	 * 
+	 * @param name - the ingredient name
+	 * 
+	 * @return the quantity of ingredient named {@code name}
+	 */
 	public int getIngredientQuantity(String îame) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	/**
+	 * Simulates acquiring  {@link Ingredient} from the Warehouse
+	 * 
+	 * @param name - ingredient name
+	 * @param quantity - quantity to take
+	 */
 	public void takeIngredient(String name, int quantity) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	/**
+	 * Adds a new {@link Ingredient} to the Warehouse
+	 * 
+	 * @param name - the ingredient name
+	 * @param quantity - the ingredient quantity
+	 */
 	public void addIngredient(String name, int quantity) {
 		// TODO Auto-generated method stub
 		
