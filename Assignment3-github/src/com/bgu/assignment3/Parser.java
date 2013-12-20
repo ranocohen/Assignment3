f1f3fb4711b8dc7d67e81810319523fd5cab1033
Idan <idanakav@gmail.com>
2013-12-20 21:14:49 +0200

    input files and Address class

--- Assignment3-github/src/com/bgu/assignment3/Parser.java
+++ Assignment3-github/src/com/bgu/assignment3/Parser.java
@@ -1,11 +1,51 @@
 package com.bgu.assignment3;
 
+import java.io.File;
+import java.util.Vector;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class Parser {
 
 	public static void parseMenu(String fileName, Management management) {
-		
+		 try {
+			 
+				File file = new File(fileName);
+				JAXBContext jaxbContext = JAXBContext.newInstance(Management.class);
+		 
+				Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+				Management customer = (Management) jaxbUnmarshaller.unmarshal(file);
+				System.out.println(customer);
+		 
+			  } catch (JAXBException e) {
+				e.printStackTrace();
+			  }
+	}
+
+	private static Vector<KitchenTool> parseKitchenTools(NodeList list) {
+		Vector<KitchenTool> kt = new Vector<KitchenTool>();
+		for (int temp = 0; temp < list.getLength(); temp++) {
+			Node nNode = list.item(temp);
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+
+				String name = eElement.getElementsByTagName("name").item(0)
+						.getTextContent();
+				String quantityStr = eElement.getElementsByTagName("quantity")
+						.item(0).getTextContent();
+				int quantity = Integer.valueOf(quantityStr);
+				 
+				kt.add(new KitchenTool(name, quantity));
+			}
+
+		}
+		return kt;
 	}
 }
-
-
