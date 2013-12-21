87ed065a04f1d09effdc5981daa5197cbf3a0328
Idan <idanakav@gmail.com>
2013-12-21 17:36:48 +0200

    ignoring interface in jaxb

--- Assignment3-github/src/com/bgu/assignment3/actions/RunnableChef.java
+++ Assignment3-github/src/com/bgu/assignment3/actions/RunnableChef.java
@@ -1,8 +1,14 @@
 package com.bgu.assignment3.actions;
 
+import java.util.Vector;
+import java.util.concurrent.Future;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import com.bgu.assignment3.passives.Order;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class RunnableChef implements Runnable {
@@ -14,6 +20,8 @@ public class RunnableChef implements Runnable {
 	@XmlElement(name="enduranceRating")
 	private double endurance;
 	
+	@XmlTransient
+	private Vector<Future<Order>> ordersInProgress;
 	
 	public void run() {
 		
