9589b33542887864b18878dd4a8782ba20b5c5dc
Idan <idanakav@gmail.com>
2013-12-21 16:10:37 +0200

    unmarshall listener added(not working correctly)

--- Assignment3-github/src/com/bgu/assignment3/passives/Orders.java
+++ Assignment3-github/src/com/bgu/assignment3/passives/Orders.java
@@ -1,5 +1,6 @@
 package com.bgu.assignment3.passives;
 
+import java.util.Iterator;
 import java.util.Vector;
 
 import javax.xml.bind.annotation.XmlElement;
@@ -8,14 +9,25 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "OrderList")
 public class Orders {
-
+	
+	Iterator<Order> iterator;
+	
 	@XmlElementWrapper(name="Orders")
 	@XmlElement(name = "Order")
 	private Vector<Order> orders;
 
-	public void getNextOrder() {
+	public Orders() {
+		iterator = orders.iterator();
+	}
+	
+	
+
+	public Order getNextOrder() {
 		
+		if(iterator.hasNext())
+			return iterator.next();
 		
+		return null;
 	}
 	
 }
