5ca4787dbcf16bcd9c256f4d8e3746fb6a50108d
Idan <idanakav@gmail.com>
2013-12-21 03:22:04 +0200

    All parsers done , had to add 2 classes , which actually make a good OOP design

---
+++ Assignment3-github/src/com/bgu/assignment3/passives/Staff.java
@@ -0,0 +1,23 @@
+package com.bgu.assignment3.passives;
+
+import java.util.Vector;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+
+import com.bgu.assignment3.actions.RunnableChef;
+import com.bgu.assignment3.actions.RunnableDeliveryPerson;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Staff {
+
+	@XmlElementWrapper(name="Chefs")
+	@XmlElement(name = "Chef")
+	private Vector<RunnableChef> chefs;
+	
+	@XmlElementWrapper(name="DeliveryPersonals")
+	@XmlElement(name = "DeliveryPerson")
+	private Vector<RunnableDeliveryPerson> deliveryPersons;
+}
