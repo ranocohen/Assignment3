5e00674f8916d5d0340dd629bb09a6c42edd8d3e
ranocohen <ranocohen@gmail.com>
2013-12-30 21:24:01 +0200

    ingredient in statistics print fixed

--- Assignment3-github/src/com/bgu/assignment3/passives/Statistics.java
+++ Assignment3-github/src/com/bgu/assignment3/passives/Statistics.java
@@ -1,6 +1,8 @@
 package com.bgu.assignment3.passives;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Statistics {
 
@@ -9,17 +11,27 @@ public class Statistics {
 		private static ArrayList<Order> deliveredOrders;
 		private static ArrayList<Ingredient> consumedIngredients;
 		private static double moneyGained;
-		
+
+		// Initialize ConcurrentHashMap instance
+		static ConcurrentHashMap<Ingredient, Integer> m;
+
 		public static void init() {
 			consumedIngredients = new ArrayList<Ingredient>();
 			deliveredOrders = new ArrayList<Order>();
+			m = new ConcurrentHashMap<Ingredient, Integer>();
 		}
 
-		
 		public static void addIngredientToStatistic(Ingredient ing) {
-			for(Ingredient ingredient : consumedIngredients) {
-				if(ingredient.compareTo(ing) == 0 )
-					return;		
+
+			if (m.containsKey(ing)) {
+				m.put(ing, m.get(ing) + 1);
+			} else {
+				m.put(ing, 1);
+			}
+			
+			for (Ingredient ingredient : consumedIngredients) {
+				if (ingredient.compareTo(ing) == 0)
+					return;
 			}
 			consumedIngredients.add(ing);
 		}
@@ -29,20 +41,26 @@ public class Statistics {
 		}
 
 		public static void addMoneyGained(double moneyGained) {
-			StatisticsClass.moneyGained+= moneyGained;
+			StatisticsClass.moneyGained += moneyGained;
 		}
-		
+
 		@Override
 		public String toString() {
 			StringBuilder builder = new StringBuilder();
-			builder.append("Money gained: "+moneyGained);
-			
-			for(Ingredient ingredient : consumedIngredients) {
-				builder.append(ingredient.toString() +"\n");
+			builder.append("Money gained: " + moneyGained);
+
+			for (Ingredient ingredient : consumedIngredients) {
+				builder.append(ingredient.toString() + "\n");
+			}
+
+			Iterator<Ingredient> keySetIterator = m.keySet().iterator();
+
+			while (keySetIterator.hasNext()) {
+				Ingredient key = keySetIterator.next();
+				System.out.println("key: " + key.getName() + " value: " + m.get(key));
 			}
 			return builder.toString();
 		}
 	}
 
-	
 }
